8504cdb698adad8b3011daf4433745b215fa1277
Martin Säfdal <martin.safdal@opsdis.com>
2019-07-31 08:42:49 +0200

    Reworked perfdict

--- getperfdata.py
+++ getperfdata.py
@@ -13,22 +13,24 @@ user = 'monitor'
 password = 'monitor'
 query = '[services]%20all&columns=host.name,description,perf_data'
 
-r = requests.get('https://' + url + '/api/filter/query?query=' + query + '', auth=HTTPBasicAuth(user, password), verify=False, headers={'Content-Type' : 'application/json'})
+r = requests.get('https://' + url + '/api/filter/query?query=' + query, auth=HTTPBasicAuth(user, password), verify=False, headers={'Content-Type' : 'application/json'})
 y = json.loads(r.content)
 
 labeldict = {}
+perfdict = {}
 
 length = len(y)
 print(length)
 for i in range(length):
-    #print(y[i]['host']['name'])
     labeldict.update({'hostname': y[i]['host']['name']})
-    #print(y[i]['description'])
-    labeldict.update({'service': y[i]['description']})
+    #labeldict.update({'service': y[i]['description']})
     lenny = len(y[i]['perf_data'])
-    print(lenny)
-    for ds in range(lenny):
-        if ds != 0:
-            print(y[i]['perf_data'])
+    perfd = y[i]['perf_data']
+    if lenny != 0:
+        print(perfd)
+        for k, v in perfd.items():
+            print(k)
+            print(v)
+
 
 print(labeldict)
