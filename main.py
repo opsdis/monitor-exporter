fb3ef3d531af922a9f81dbd174acecbb21aebdc3
Martin Säfdal <martin.safdal@opsdis.com>
2019-07-31 10:14:23 +0200

    Created perfdata class

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+- Din tjänst skall kunna ta en emot en filter query
+- Gör anrop mot Monitor över apiet med en filter query
+- Retunera tillbaka en dictionary av perfdata samt en dictionary av labels
+- Labels är allt annat som frågan filter retunerar som inte är perfdata tex hostname, servicename, ...
+"""
+from perfdata import Perfdata
+
+
+class_test = Perfdata('monitor.aw.oc-testbench.xyz', 'monitor', 'monitor', '[services]%20all&columns=host.name,description,perf_data')
+
+class_test.get_data()
+
+print(class_test.get_perfdata())
---
+++ perfdata.py
@@ -0,0 +1,36 @@
+import requests, urllib3, json
+from requests.auth import HTTPBasicAuth
+
+# Disable InsecureRequestWarning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class Perfdata:
+    def __init__(self, url, user, password, query):
+        self.url = url
+        self.user = user
+        self.password = password
+        self.query = query
+
+    def get_data(self):
+        perfdata_from_monitor = requests.get('https://' + self.url + '/api/filter/query?query=' + self.query, auth=HTTPBasicAuth(self.user, self.password), verify=False, headers={'Content-Type' : 'application/json'})
+        self.perfdata_json = json.loads(perfdata_from_monitor.content)
+        return self.perfdata_json
+
+    def get_perfdata(self):
+        perfdict = {}
+
+        length = len(self.perfdata_json)
+
+        for i in range(length):
+            length_perfdata = len(self.perfdata_json[i]['perf_data'])
+            perfd = self.perfdata_json[i]['perf_data']
+            if length_perfdata != 0:
+                #print(perfd)
+                for k, v in perfd.items():
+                    #print(k)
+                    for nestk, nestv in v.items():
+                        newkey = self.perfdata_json[i]['description'] + '_' + k + '_' + nestk
+                        newkey = newkey.replace(' ', '_')
+                        newkey = newkey.lower()
+                        perfdict.update({newkey: nestv})
+        return perfdict
