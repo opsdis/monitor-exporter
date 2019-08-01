fe962dbc16d24acd4f092910da8ab8127eab042f
Martin Säfdal <martin.safdal@opsdis.com>
2019-08-01 17:39:02 +0200

    custom_vars to labels

--- perfdata.py
+++ perfdata.py
@@ -9,16 +9,40 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 class Perfdata:
     def __init__(self, query_hostname):
         monitor = monitorconnection.MonitorConfig()
-        self.url = 'https://' + monitor.get_host() + '/api/filter/query?query=[services]%20host.name="' + query_hostname + '"&columns=host.name,description,perf_data,check_command'
+        self.query_hostname = query_hostname
+        self.url = 'https://' + monitor.get_host() + '/api/filter/query?query=[services]%20host.name="' + self.query_hostname + '"&columns=host.name,description,perf_data,check_command'
         self.user = monitor.get_user()
         self.passwd = monitor.get_passwd()
+
+
+    def get_custom_vars(self):
+        monitor = monitorconnection.MonitorConfig()
+        url = 'https://' + monitor.get_host() + '/api/filter/query?query=[hosts]%20display_name="' + self.query_hostname + '"&columns=custom_variables'
+
+        custom_vars_from_monitor = requests.get(url, auth=HTTPBasicAuth(self.user, self.passwd), verify=False, headers={'Content-Type': 'application/json'})
+        custom_vars_json = json.loads(custom_vars_from_monitor.content)
+
+        self.custom_vars = {}
+        for var in custom_vars_json:
+            self.custom_vars = var['custom_variables']
+
+        return self.custom_vars
 
     def _get_data(self):
         data_from_monitor = requests.get(self.url, auth=HTTPBasicAuth(self.user, self.passwd), verify=False, headers={'Content-Type': 'application/json'})
         self.data_json = json.loads(data_from_monitor.content)
+
         ExporterLog.info('API call: ' + data_from_monitor.url)
-        ExporterLog.info('Status code: ' + str(data_from_monitor.status_code))
+
+        if data_from_monitor.status_code == 401:
+            ExporterLog.error('Status code: ' + str(data_from_monitor.status_code))
+            ExporterLog.error(self.data_json['error'])
+            ExporterLog.error(self.data_json['full_error'])
+        else:
+            ExporterLog.info('Status code: ' + str(data_from_monitor.status_code))
+
         ExporterLog.info('Elapsed time: ' + str(data_from_monitor.elapsed))
+
         if len(data_from_monitor.content) > 2:
             ExporterLog.info('Received perfdata from Monitor')
         else:
@@ -28,6 +52,16 @@ class Perfdata:
     def get_perfdata(self):
         monitor = monitorconnection.MonitorConfig()
         self._get_data()
+        
+        labels = monitor.get_labels()
+        monitor_custom_vars = Perfdata(self.query_hostname).get_custom_vars()
+        new_labels = {}
+        if len(monitor_custom_vars) > 0:
+            monitor_custom_vars = {k.lower(): v for k, v in monitor_custom_vars.items()}
+            for i in labels.keys():
+                if i in monitor_custom_vars.keys():
+                    new_labels.update({labels[i]: monitor_custom_vars[i]})
+
         self.perfdatadict = {}
         check_command_regex = re.compile(r'^.+?[^!\n]+')
 
@@ -58,7 +92,14 @@ class Perfdata:
                         prometheus_key = prometheus_key.replace(' ', '_')
                         prometheus_key = prometheus_key.replace('/', 'slash')
                         prometheus_key = prometheus_key.replace('%', 'percent')
-                        prometheus_key = prometheus_key + '{hostname="' + item['host']['name'] + '"' + ', service="' + item['description'] + '"}'
+                        if new_labels == None:
+                            prometheus_key = prometheus_key + '{hostname="' + item['host']['name'] + '"' + ', service="' + item['description'] + '"}'
+                        else:
+                            labelstring = ''
+                            for label_key, label_value in new_labels.items():
+                                labelstring += ', ' + label_key + '="' + label_value + '"'
+                            prometheus_key = prometheus_key + '{hostname="' + item['host']['name'] + '"' + ', service="' + item['description'] + '"' + labelstring + '}'
+
                         self.perfdatadict.update({prometheus_key: str(nested_value)})
 
         return self.perfdatadict
