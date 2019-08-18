dfeb325c6e5cf54083d23b2353017d13bda88e58
andersh <anders.haal@ingby.com>
2019-08-18 22:14:06 +0200

    Update to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='monitor-exporter',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     author='thenodon',
     author_email='anders@opsdis.com',
